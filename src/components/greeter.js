41f63f334bd1f69cfae9b068cf5bbb7ffeef18e8
Miguel Julio <fitfab@yahoo.com>
2016-02-15 16:32:15 -0500

    add style and css loader

--- src/components/greeter.js
+++ src/components/greeter.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import config from '../data/config.json';
 
+import './greet.css';
+
 export default React.createClass({
   render() {
     return (
-      <div>
+      <div className="greet-msg">
         {config.greetText}
       </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,11 @@ module.exports = {
                 "test": /\.js$/,
                 "exclude": /node_modules/,
                 "loader": "babel"
-              }
+            },
+            {
+                "test": /\.css$/,
+                "loader": 'style!css'
+            }
         ]
     },
     "devServer": {
